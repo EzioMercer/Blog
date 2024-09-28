f605c2fb491a7b0322114ecc7b679e440e014ae8
EzioMercer <rovsanbadirxanov@gmail.com>
2024-09-28 23:37:31 +0400

    Test metadataBase

--- app/_utils/generateMetadata.ts
+++ app/_utils/generateMetadata.ts
@@ -7,19 +7,19 @@ const generateMetadata = (frontmatter: MyMetadata): Metadata => {
 		description,
 	} = frontmatter;
 
-	const productionLinkPrefix = 'https://EzioMercer.github.io/Blog/';
+	const productionLinkPrefix = 'https://EzioMercer.github.io/Blog';
 
 	return {
 		...frontmatter,
 		title: `${title} | EzioMercer`,
+		metadataBase: new URL(productionLinkPrefix),
 		openGraph: {
 			title: title as string,
 			description: description as string,
 			type: 'article',
-			images: productionLinkPrefix + 'favicon.ico',
 			authors: ['EzioMercer'],
 			publishedTime: frontmatter.publishDate,
-			tags: frontmatter.tags.split(', '),
+			tags: frontmatter.tags.split(', ')
 		},
 		other: {
 			'giscus:backlink': productionLinkPrefix + title
